e4d8df9b4774a0c04ebde72dbc4a5ef2f4cf4bed
Hinny <henning.roos@gmail.com>
2021-03-03 23:30:19 +0100

    Change ReadLine to ReadKey

--- RunewarsBattleSimulation/Program.cs
+++ RunewarsBattleSimulation/Program.cs
@@ -10,7 +10,7 @@ namespace RunewarsBattleSimulation
     {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
