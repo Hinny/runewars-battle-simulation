c76520c8d3c805f6d59311d3bd188a17f8ed08c3
Hinny <henning.roos@gmail.com>
2023-11-19 02:35:31 +0100

    Refactor and prints

--- runewars.py
+++ runewars.py
@@ -7,19 +7,20 @@ daqan_lords = Faction("Daqan Lords")
 latari_elves = Faction("Latari Elves")
 
 daqan_lords.add_unit_type(Bowman(2))
-daqan_lords.add_unit_type(Footman(2))
 daqan_lords.add_unit_type(Knight(2))
-daqan_lords.add_unit_type(SiegeTower(1))
+daqan_lords.add_unit_type(Footman(2))
 daqan_lords.add_unit_type(Hellhound(1))
+daqan_lords.add_unit_type(SiegeTower(1))
 
 latari_elves.add_unit_type(Archer(4))
+latari_elves.add_unit_type(Razorwing(1))
 latari_elves.add_unit_type(PegasusRider(1))
 latari_elves.add_unit_type(Sorceress(1))
 latari_elves.add_unit_type(Warrior(1))
-latari_elves.add_unit_type(Razorwing(1))
+
 
 fate_deck = FateDeck()
 
 battle_simulator = BattleSimulator(daqan_lords, latari_elves, fate_deck)
 
-battle_simulator.battle()
+battle_simulator.resolve_battle()
