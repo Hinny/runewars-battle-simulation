b97991b441f477a6a6347cf52d17352226392955
Hinny <henning.roos@gmail.com>
2023-11-28 00:03:02 +0100

    More special abilites

--- players.py
+++ players.py
@@ -11,7 +11,13 @@ class PlayerInterface:
     def choose_unit_type(self, unit_types):
         raise NotImplementedError
 
-    def choose_unit(self, eligible_units):
+    def choose_unit(self, units):
+        raise NotImplementedError
+
+    def choose_two_units(self, units):
+        raise NotImplementedError
+
+    def choose_option(self, *option_strings):
         raise NotImplementedError
 
 class HumanPlayer(PlayerInterface):
@@ -22,6 +28,14 @@ class HumanPlayer(PlayerInterface):
     def choose_unit(self, units):
         print(f"          {self.faction.name}, choose a unit:")
         return self.choose_unit_from_list(units)
+
+    def choose_two_units(self, units):
+        print(f"          {self.faction.name}, choose the first of two different units:")
+        first_unit = self.choose_unit_from_list(units)
+        remaining_units = [unit for unit in units if unit != first_unit]
+        print(f"          {self.faction.name}, choose the second unit:")
+        second_unit = self.choose_unit_from_list(remaining_units)
+        return first_unit, second_unit
 
     def choose_unit_type_from_list(self, unit_types):
         for i, unit_type in enumerate(unit_types):
@@ -35,17 +49,25 @@ class HumanPlayer(PlayerInterface):
         choice = self.get_user_choice(len(units))
         return units[choice - 1]
 
+    def choose_option(self, *option_strings):
+        print(f"          {self.faction.name}, choose an option:")
+        for i, string in enumerate(option_strings):
+            print(f"           {i+1}. {string}")
+
+        choice = self.get_user_choice(len(option_strings))
+        return choice
+
     def get_user_choice(self, max_number):
         valid_choice = False
         while not valid_choice:
             try:
-                choice = int(input("Your choice: "))
+                choice = int(input("          Your choice: "))
                 if 1 <= choice <= max_number:
                     valid_choice = True
                 else:
-                    print("Invalid choice, try again.")
+                    print("          Invalid choice, try again.")
             except ValueError:
-                print("Invalid input, try again.")
+                print("          Invalid input, try again.")
         return choice
 
 class RandomAIPlayer(PlayerInterface):
@@ -54,3 +76,12 @@ class RandomAIPlayer(PlayerInterface):
 
     def choose_unit(self, units):
         return random.choice(units)
+
+    def choose_two_units(self, units):
+        first_unit = random.choice(units)
+        remaining_units = [unit for unit in units if unit != first_unit]
+        second_unit = random.choice(remaining_units)
+        return first_unit, second_unit
+
+    def choose_option(self, *option_strings):
+        return random.randint(1, len(option_strings))
